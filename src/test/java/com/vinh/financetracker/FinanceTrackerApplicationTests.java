e30df3a841b2e6b37e53369f489ac2029a7773e8
QuangVinh170220005 <quangvinhabcdg@gmail.com>
2026-03-26 22:56:23 +0700

    feat: feat: integrate Testcontainers with PostgreSQL for testing

--- src/test/java/com/vinh/financetracker/FinanceTrackerApplicationTests.java
+++ src/test/java/com/vinh/financetracker/FinanceTrackerApplicationTests.java
@@ -2,12 +2,21 @@ package com.vinh.financetracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers // Kích hoạt Testcontainers
 class FinanceTrackerApplicationTests {
+
+	@Container
+	@ServiceConnection // Tự động map các thuộc tính spring.datasource.*
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
 
 	@Test
 	void contextLoads() {
+		// Nếu chạy vào đây được là thành công
 	}
-
 }
